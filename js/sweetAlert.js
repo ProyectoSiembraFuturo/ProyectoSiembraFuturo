50a09228d3c590f87b5304f430e2e5db06fb9cab
ProyectoSiembraFuturo <105892878+ProyectoSiembraFuturo@users.noreply.github.com>
2022-06-08 21:41:42 -0500

    Add files via upload

---
+++ js/sweetAlert.js
@@ -0,0 +1,115 @@
+function recep() {
+    Swal.fire({
+        title: 'Recepción ',
+        text: 'Plantas de Cultivo.',
+        imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjmI33XfbdEPElgYuqZs9ZzojKml4sw275dux3KdIxxhnDKdy6_jwyOnZFoZHO2JREUB_72gJHsuT6rQbTE6HbXIEZdg4vFoDCfxlaiIpwHf6DZjmSRljeK_ERuPOqJ6SzFVhHYnxDf_rp88Y1L3sSeMb_qHUu8nOsE_meHg8hro_7lofHW-Fe_OehUPg/s320/melina.png',
+        imageWidth: 400,
+        confirmButtonText: 'Cerrar',
+        showCloseButton: 'true',
+
+        // title:
+        // text:
+        // html:
+        // icon:
+        // confirmButtonText:
+        // footer:
+        // width:
+        // padding:
+        // background:
+        // grow:
+        // backdrop:
+        // timer:
+        // timerProgressBar:
+        // toast:
+        // position:
+        // allowOutsideClick:
+        // allowEscapeKey:
+        // allowEnterKey:
+        // stopKeydownPropagation:
+    
+        // input:
+        // inputPlaceholder:
+        // inputValue:
+        // inputOptions:
+        
+        //  customClass:
+        // 	container:
+        // 	popup:
+        // 	header:
+        // 	title:
+        // 	closeButton:
+        // 	icon:
+        // 	image:
+        // 	content:
+        // 	input:
+        // 	actions:
+        // 	confirmButton:
+        // 	cancelButton:
+        // 	footer:	
+    
+        // showConfirmButton:
+        // confirmButtonColor:
+        // confirmButtonAriaLabel:
+    
+        // showCancelButton:
+        // cancelButtonText:
+        // cancelButtonColor:
+        // cancelButtonAriaLabel:
+        
+        // buttonsStyling:
+        // showCloseButton:
+        // closeButtonAriaLabel:
+    
+    
+        // imageUrl:
+        // imageWidth:
+        // imageHeight:
+        // imageAlt:
+    
+      });
+
+ 
+};
+
+
+function melina() {
+    Swal.fire({
+        title: 'Melina ',
+        text: 'Plantas de Melina.',
+        imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh7h_1WaGUBigezYM50gJRefwBKOAE-cxPeGH5QscQwzL-Mjhr_JuzzO1GIQ9UT4lDNwNIdnVxBvpAZUP2JKrs4AiML9ghZNYqr7tCW6jyhWp0mmmgnHUgRRG8ruM-wNzFCmDEOq46UERq4tpfMMgbjexY6y5QvGDLy3r7KXdqCzGg6y323kYzTwITHqA/s320/ftmelina2.png',
+        imageWidth: 400,
+        confirmButtonText: 'Cerrar',
+        showCloseButton: 'true',
+
+      });
+
+ 
+}
+
+function cardamomo() {
+    Swal.fire({
+        title: 'Cardamomo',
+        text: 'Cultivo de Cardamomo.',
+        imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiHwlJfD8M_r0IBJv2AvD2XpMe6DbzFyRn1BGdDbN-1kJOkff4DPaMQM2R9v0jQjGB_lymBDA_G2P6V-a07DADlytpFyekoKydQH8V9huZkKhpa6FnC4qMZLXvdgYfuCIal4SS4FChEY8rZnXWs5gLionNMT5NDnvYrl9rQ8Wvy4jKWUt7_iXWOOMpJhg/s320/cardam.png',
+        imageWidth: 400,
+        confirmButtonText: 'Cerrar',
+        showCloseButton: 'true',
+
+      });
+
+ 
+}
+
+function cardamomo2() {
+    Swal.fire({
+        title: 'Semillas',
+        text: 'Cardamomo para Cultivar.',
+        imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjQEsUaqpZCKtktyyLd6jlRc6VwACpfrZ37C5fjzHKHD2xGERhZxz46dXNVmUoh-zl2GXOhO6CjkQmXPwKKWzZQ3yxBraFrtn0IJiYFrdOIGJJEiFbKN1GmIZg_z1ioRxy1RKqUaIpN4BEx6USgZPDqimpmePJV9GnQx5jBfmk86lF2KLeDHuYQZ4RXjg/s320/cardamomo2.png',
+        imageWidth: 400,
+        confirmButtonText: 'Cerrar',
+        showCloseButton: 'true',
+
+      });
+
+ 
+}
